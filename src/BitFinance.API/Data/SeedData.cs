684aff3a0c60a11aa9b8a9b5e60a52d3edc08881
Gustavo Miranda <gmiranda90@outlook.com>
2023-11-18 19:02:47 -0300

    Add database seed

---
+++ src/BitFinance.API/Data/SeedData.cs
@@ -0,0 +1,95 @@
+using BitFinance.API.Enums;
+using BitFinance.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BitFinance.API.Data;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var dbContext = new ApplicationDbContext(
+                   serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+        {
+            // Look for any Bills.
+            if (dbContext.Bills.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            PopulateTestData(dbContext);
+        }
+    }
+    
+    private static void PopulateTestData(ApplicationDbContext dbContext)
+    {
+        foreach (var item in dbContext.Bills)
+        {
+            dbContext.Remove(item);
+        }
+        dbContext.SaveChanges();
+
+        Bill bill1 = new()
+        {
+            Name = "Vivo Fibra",
+            Category = BillCategory.Utilities,
+            CreatedDate = DateTime.UtcNow,
+            DueDate = DateTime.UtcNow,
+            AmountDue = 120M,
+            IsPaid = false
+        };
+        
+        Bill bill2 = new()
+        {
+            Name = "Cagece",
+            Category = BillCategory.Utilities,
+            CreatedDate = DateTime.UtcNow,
+            DueDate = DateTime.UtcNow,
+            PaidDate = DateTime.UtcNow,
+            AmountDue = 80M,
+            AmountPaid = 80M,
+            IsPaid = true
+        };
+        
+        Bill bill3 = new()
+        {
+            Name = "Unimed CE",
+            Category = BillCategory.Healthcare,
+            CreatedDate = DateTime.UtcNow,
+            DueDate = DateTime.UtcNow,
+            PaidDate = new DateTime(2023, 11, 18).ToUniversalTime(),
+            AmountDue = 350M,
+            AmountPaid = 350M,
+            IsPaid = true
+        };
+        
+        Bill bill4 = new()
+        {
+            Name = "Allianz Seguros",
+            Category = BillCategory.Insurance,
+            CreatedDate = DateTime.UtcNow,
+            DueDate = new DateTime(2023, 12, 10).ToUniversalTime(),
+            AmountDue = 1000M,
+            IsPaid = false
+        };
+        
+        Bill bill5 = new()
+        {
+            Name = "Netflix",
+            Category = BillCategory.Entertainment,
+            CreatedDate = DateTime.UtcNow,
+            DueDate = new DateTime(2023, 12, 01).ToUniversalTime(),
+            AmountDue = 49.90M,
+            IsPaid = false,
+            IsDeleted = true
+        };
+
+        dbContext.Bills.Add(bill1);
+        dbContext.Bills.Add(bill2);
+        dbContext.Bills.Add(bill3);
+        dbContext.Bills.Add(bill4);
+        dbContext.Bills.Add(bill5);
+
+        dbContext.SaveChanges();
+    }
+}
