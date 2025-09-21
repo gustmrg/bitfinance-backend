b66f125e880b32b00ebf031dfb6d6b4796d3d280
Gustavo Miranda <gustmrg@gmail.com>
2025-09-21 19:38:35 -0300

    feat: add timezone to organization and user entities

--- src/BitFinance.Business/Entities/User.cs
+++ src/BitFinance.Business/Entities/User.cs
@@ -8,6 +8,7 @@ public class User : IdentityUser
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public List<Organization> Organizations { get; } = [];
+    public UserSettings Settings { get; set; } = new();
     
     [NotMapped]
     public string FullName => $"{FirstName} {LastName}";
